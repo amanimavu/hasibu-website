d4bd0a22d8d04b1913de02e44cb98a6890391328
amanimavu <amanimavu@gmail.com>
2026-06-18 15:12:29 +0300

    feat: add astro icons package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   // SSG. Set real domain for correct sitemap/canonical URLs.
   site: "https://hasibu.africa",
   output: "static",
-  integrations: [react()],
+  integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
